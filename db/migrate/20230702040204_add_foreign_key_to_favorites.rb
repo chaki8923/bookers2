fe7a7c169c4579d3e39735598bb544aa1f1a26ef
EC2 Default User <ec2-user@ip-172-31-69-247.ec2.internal>
2023-07-09 03:17:20 +0000

    migration修正完了

--- db/migrate/20230702040204_add_foreign_key_to_favorites.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToFavorites < ActiveRecord::Migration[6.1]
-  def change
-    add_foreign_key :favorites, :users
-    add_foreign_key :favorites, :books
-  end
-end
--- db/migrate/20230702040613_add_foreign_key_to_book_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToBookComments < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :book_comments, users
-    remove_foreign_key :book_comments, books
-  end
-end
