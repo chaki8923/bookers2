2dcd2a63006f2d43a98c4beee153eac3602ba2bb
EC2 Default User <ec2-user@ip-172-31-69-247.ec2.internal>
2023-08-19 07:41:49 +0000

    filter前

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,21 @@ class User < ApplicationRecord
   has_many :books,  dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  # フォローする側関連付け
-  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  
   # フォローされる側関連付け
   has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
-  
-  # follower_relationships経由でfollowedと多対多の関係
-  has_many :followings, through: :follower_relationships, source: :followed
   # followed_relationships経由でfollowerと多対多の関係
   has_many :followers, through: :followed_relationships, source: :follower
+  
+  
+  
+  # フォローする側関連付け
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  # follower_relationships経由でfollowedと多対多の関係
+  has_many :followings, through: :follower_relationships, source: :followed
+  
+  
+  
   
   has_many :group_users
   has_many :groups, through: :group_users
